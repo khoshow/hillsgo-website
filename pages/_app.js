71e81a71f8c3ef72d7a049d40b7e444f36b087bb
khoshow <khoshow.official@gmail.com>
2024-10-29 14:54:20 +0530

    aa

--- pages/_app.js
+++ pages/_app.js
@@ -6,32 +6,32 @@ import nookies from "nookies";
 import Header from "../components/Header"; // Import the Header component
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const router = useRouter();
+  // const [loading, setLoading] = useState(true);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setIsAuthenticated(!!user);
-      setLoading(false);
-    });
+  // useEffect(() => {
+  //   const unsubscribe = auth.onAuthStateChanged((user) => {
+  //     setIsAuthenticated(!!user);
+  //     setLoading(false);
+  //   });
 
-    return () => unsubscribe();
-  }, []);
+  //   return () => unsubscribe();
+  // }, []);
 
-  useEffect(() => {
-    if (
-      !loading &&
-      !isAuthenticated &&
-      router.pathname.startsWith("/protected")
-    ) {
-      router.push("/auth/signin");
-    }
-  }, [loading, isAuthenticated, router.pathname]);
+  // useEffect(() => {
+  //   if (
+  //     !loading &&
+  //     !isAuthenticated &&
+  //     router.pathname.startsWith("/protected")
+  //   ) {
+  //     router.push("/auth/signin");
+  //   }
+  // }, [loading, isAuthenticated, router.pathname]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
 
   return (
     <>
