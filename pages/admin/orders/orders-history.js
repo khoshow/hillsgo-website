96ab1f053d6add1a113831e10920713c12285171
khoshow <khoshow.official@gmail.com>
2025-03-13 22:53:39 +0530

    order history trial

--- pages/admin/orders/orders-history.js
+++ pages/admin/orders/orders-history.js
@@ -20,55 +20,57 @@ import { colors } from "@/data/colors";
 import ImageSlider from "../../../components/sliders/ImageSliders";
 import { useUser } from "../../../contexts/UserContext"; // Import your UserContext
 import Header from "@/components/Header";
-import DriverLayout from "@/components/layout/DriverLayout"; // Assuming you have a layout for Estore
-import Driver from "@/components/auth/Driver";
+import AdminLayout from "@/components/layout/AdminLayout"; // Assuming you have a layout for Estore
 import Admin from "@/components/auth/Admin";
-import AdminLayout from "@/components/layout/AdminLayout";
+import Image from "next/image";
 
-export default function MyPosts() {
+export default function MyItems() {
   const { user, loading: userLoading } = useUser(); // Access the user context
-  const [posts, setPosts] = useState([]);
+  const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
   const storage = getStorage();
 
   useEffect(() => {
     if (userLoading) return;
-    const fetchPosts = async () => {
+    const fetchItems = async () => {
       if (!user) {
         router.push("/"); // Redirect if not logged in
         return;
       }
 
       try {
-        const postsQuery = query(
-          collection(db, "driversPosts"),
-          where("driverId", "==", user.uid) // Fetch products created by the logged-in user
+        const itemsQuery = query(
+          collection(db, "ordersHistory")
+          // where("driver.driverId", "==", user.uid) // Fetch products created by the logged-in user
         );
 
-        const querySnapshot = await getDocs(postsQuery);
-        const postsList = querySnapshot.docs.map((doc) => ({
+        const querySnapshot = await getDocs(itemsQuery);
+        const itemsList = querySnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
 
-        setPosts(postsList); // Update state with fetched products
+        setItems(itemsList); // Update state with fetched products
       } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error("Error fetching items:", error);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchPosts();
+    fetchItems();
   }, [user, router]);
-  const handleDelete = async (postId, imageUrls) => {
-    const confirmed = confirm("Are you sure you want to delete this post?");
+
+  console.log("items", items);
+
+  const handleDelete = async (itemId, imageUrls) => {
+    const confirmed = confirm("Are you sure you want to delete this item?");
     if (!confirmed) return;
 
     try {
       // Delete product document from Firestore
-      await deleteDoc(doc(db, "driversPosts", postId));
+      await deleteDoc(doc(db, "driversItems", itemId));
       // Delete each image from Firebase Storage
       if (imageUrls) {
         const deletePromises = imageUrls.map(async (url) => {
@@ -83,12 +85,12 @@ export default function MyPosts() {
       }
 
       // Update local state to remove deleted product
-      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
+      setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
 
-      alert("Post deleted successfully!");
+      alert("Item deleted successfully!");
     } catch (error) {
-      console.error("Error deleting post:", error);
-      alert("Failed to delete the post.");
+      console.error("Error deleting item:", error);
+      alert("Failed to delete the item.");
     }
   };
   if (loading)
@@ -106,48 +108,165 @@ export default function MyPosts() {
       <AdminLayout>
         <Header />
         <div style={styles.container}>
-          <h1 style={styles.heading}>My Posts</h1>
+          <h1 style={styles.heading}>My Delivered Items</h1>
           <div style={styles.productGrid}>
-            {posts.length > 0 ? (
-              posts.map((post) => (
-                <div key={post.id} style={styles.productCard}>
-                  <ImageSlider images={post.images} />
-                  <p>{post.content}</p>
+            {items.length > 0 ? (
+              items.map((item) => (
+                <div key={item.id} style={styles.productCard}>
+                  <div style={styles.tableContainerBox}>
+                    <div style={styles.tableContainer}>
+                      {/* <img
+                        src="https://firebasestorage.googleapis.com/v0/b/hillsgodev.appspot.com/o/estoreProducts%2F1734802647603_dd.jpg?alt=media&token=719f9c2e-111a-4266-addc-86c98d0508ee"
+                        alt="E-store Product"
+                        style="max-width: 300px; height: auto;"
+                      /> */}
 
-                  {/* Display all images */}
+                      <h2>Order Details</h2>
+                      <table>
+                        <tr>
+                          <th>Order ID</th>
+                          <td>ORDER-LCUXK4AR</td>
+                        </tr>
+                        <tr>
+                          <th>Delivery Code</th>
+                          <td>5411</td>
+                        </tr>
+                        <tr>
+                          <th>Status</th>
+                          <td>Completed</td>
+                        </tr>
+                        <tr>
+                          <th>Ordered Date</th>
+                          <td>08/08/2025</td>
+                        </tr>
+                        <tr>
+                          <th>Date of Delivery</th>
+                          <td>09/09/2025</td>
+                        </tr>
+                      </table>
+                    </div>
+                    <div style={styles.tableContainer}>
+                      <h3>User Details</h3>
+                      <table>
+                        <tr>
+                          <th>Name</th>
+                          <td>Khoshow</td>
+                        </tr>
+                        <tr>
+                          <th>Phone</th>
+                          <td>9873411884</td>
+                        </tr>
+                        <tr>
+                          <th>Email</th>
+                          <td>khoshow.developer@gmail.com</td>
+                        </tr>
+                        <tr>
+                          <th>Delivery Address</th>
+                          <td>Hha</td>
+                        </tr>
+                      </table>
+                    </div>
+                    <div class="table-container">
+                      <h3>Store Details</h3>
+                      <table>
+                        <tr>
+                          <th>Store Name</th>
+                          <td>Store 2</td>
+                        </tr>
+                        <tr>
+                          <th>Owner Name</th>
+                          <td>Tommy Baltimore</td>
+                        </tr>
+                        <tr>
+                          <th>Owner Email</th>
+                          <td>store2@hillsgo.com</td>
+                        </tr>
+                        <tr>
+                          <th>Store Contact</th>
+                          <td>55555</td>
+                        </tr>
+                      </table>
+                    </div>
+                    <div class="table-container">
+                      <h3>Product Details</h3>
+                      <table>
+                        <tr>
+                          <th>Product Name</th>
+                          <td>{item.product.productData.name}</td>
+                        </tr>
+                        <tr>
+                          <th>Product Description</th>
+                          <td>{item.product.productData.description}</td>
+                        </tr>
+                        <tr>
+                          <th>Weight</th>
+                          <td>{item.product.productData.weight}</td>
+                        </tr>
+                        <tr>
+                          <th>Size</th>
+                          <td>{item?.product?.productData?.size}</td>
+                        </tr>
 
-                  {/* Display all categories */}
-                  <div style={styles.categoriesContainer}>
-                    <p>Categories:</p>
-                    {post.categories && post.categories.length > 0 ? (
-                      post.categories.map((category, index) => (
-                        <span key={index} style={styles.categoryBadge}>
-                          {category}
-                        </span>
-                      ))
-                    ) : (
-                      <p>No categories assigned.</p>
-                    )}
+                        <tr>
+                          <th>Categories</th>
+                          {item?.product?.productData?.categories.map(
+                            (item, index) => (
+                              <td key={index}>{`${item}`} </td>
+                            )
+                          )}
+                        </tr>
+                      </table>
+                    </div>
+                    <div class="table-container">
+                      <h3>Driver Details</h3>
+                      <table>
+                        <tr>
+                          <th>Name</th>
+                          <td>{item.driver.name}</td>
+                        </tr>
+                      </table>
+                    </div>
+                    <div class="table-container">
+                      <h3>Pricing Details</h3>
+                      <table>
+                        <tr>
+                          <th>Price</th>
+                          <td>₹{item.product.productData.price}</td>
+                        </tr>
+
+                        <tr>
+                          <th>Quantity</th>
+                          <td>{item.product.productData.quantity}</td>
+                        </tr>
+
+                        <tr>
+                          <th>Subtotal</th>
+                          <td>₹{item.product.productData.subtotal}</td>
+                        </tr>
+                        <tr>
+                          <th>Tip</th>
+                          <td>₹{item.product.tip}</td>
+                        </tr>
+                        <tr>
+                          <th>Delivery Cost</th>
+                          <td>₹{item.product.deliveryCost}</td>
+                        </tr>
+                        <tr>
+                          <th>Total Payment</th>
+                          <td>
+                            ₹
+                            {item.product.deliveryCost +
+                              item.product.tip +
+                              item.product.productData.subtotal}
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
                   </div>
-
-                  <button
-                    style={styles.editButton}
-                    onClick={() =>
-                      router.push(`/skilled-driver/edit-post?id=${post.id}`)
-                    }
-                  >
-                    Edit
-                  </button>
-                  <button
-                    style={styles.deleteButton}
-                    onClick={() => handleDelete(post.id, post.images)}
-                  >
-                    Delete
-                  </button>
                 </div>
               ))
             ) : (
-              <p>No posts found.</p>
+              <p>No items found.</p>
             )}
           </div>
         </div>
@@ -173,13 +292,39 @@ const styles = {
     justifyContent: "center",
   },
   productCard: {
-    width: "100%",
+    // width: "50%",
     border: "1px solid #ddd",
     borderRadius: "8px",
     overflow: "hidden",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    textAlign: "center",
+    // textAlign: "center",
   },
+
+  table: {
+    width: "100%",
+    borderCollapse: "collapse",
+  },
+  tableContainerBox: {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "20px",
+    justifyContent: "spaceBetween",
+  },
+  tableContainer: {
+    flex: "1",
+    minWidth: "300px",
+  },
+  td: {
+    padding: "8px",
+    border: "1px solid #ddd",
+  },
+  th: {
+    textAlign: "left",
+    padding: "10px",
+    backgroundColor: "#f4f4f4",
+    border: "1px solid #ddd",
+  },
+
   imagesContainer: {
     display: "flex",
     flexDirection: "column",
